f6f55ddcdb3b542536a1398b9a6bc9a17f9ad276
Vu Van Tuyen <vuvantuyen19082003@gmail.com>
2025-06-05 14:23:33 +0700

    update display

--- src/Component/Display/display.jsx
+++ src/Component/Display/display.jsx
@@ -3,7 +3,7 @@ import "./display.css";
 const Display = ({ input, result, firstNum, lastNum, isResult, prevCal }) => {
   return (
     <div className="display">
-      <div>
+      <div className="math">
         {isResult ? prevCal && <div className="prev">{prevCal}</div> : ""}
       </div>
       <div>{isResult || (!firstNum && !lastNum) ? result : input}</div>
